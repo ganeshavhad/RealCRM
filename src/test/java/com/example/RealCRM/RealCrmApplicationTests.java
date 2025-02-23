c6640c5b8f696bfde2d433f09c25f726dd359de6
ganeshavhad <gavhad218@gmail.com>
2025-02-23 15:34:17 +0530

    first commit

---
+++ src/test/java/com/example/RealCRM/RealCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RealCRM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealCrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
